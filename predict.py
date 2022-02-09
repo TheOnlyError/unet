8837672d12bae932ac728d8dddcbe32ac24e1abc
Gijs <gijssieboy007@live.nl>
2022-02-09 18:10:10 +0100

    Update

--- predict.py
+++ predict.py
@@ -14,14 +14,13 @@ os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 logging.disable(logging.WARNING)
 
 def main():
-    train_dataset, validation_dataset = floorplans.load_data()
-
     unet_model = tf.keras.models.load_model('unet_model', custom_objects=custom_objects)
 
     predict = True
     if predict:
         # image = mpimg.imread('resources/single.jpg')
-        image = mpimg.imread('resources/multi_large.jpg')
+        # image = mpimg.imread('resources/multi_large.jpg')
+        image = mpimg.imread('resources/multi_largest.jpg')
         # image = cv2.resize(image, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
         image = np.expand_dims(image, axis=0)
         prediction = unet_model.predict(image)
@@ -30,6 +29,7 @@ def main():
         timestr = time.strftime("%Y%m%d-%H%M%S")
         mpimg.imsave("result" + timestr + ".jpg", result.astype(np.uint8))
     else:
+        train_dataset, validation_dataset = floorplans.load_data()
         rows = 3
         for t in range(3):
             fig, axs = plt.subplots(rows, 3, figsize=(8, rows*3))
