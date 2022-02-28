5e1454b5e280241b05cf57ba07bd1c5e867e4b3b
Unknown <gijssieboy007@live.nl>
2022-02-28 11:06:18 +0100

    Update

---
+++ train_pp.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import time
+
+import tensorflow as tf
+from tensorflow import losses, metrics
+
+from src import unet, xnet
+from src.unet import custom_objects
+from src.unet.datasets import floorplans
+from tensorflow.keras.optimizers import Adam
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+logging.disable(logging.WARNING)
+
+
+def main():
+    LEARNING_RATE = 1e-4
+    unet_model = xnet.Xnet(backbone_name='efficientnetb0', classes=3)
+
+    unet_model.compile(loss=losses.SparseCategoricalCrossentropy(),
+                  optimizer=Adam(learning_rate=LEARNING_RATE),
+                  metrics=[metrics.SparseCategoricalAccuracy()],
+                  )
+
+    # unet_model = tf.keras.models.load_model('unet_model', custom_objects=custom_objects) # 160 + 80
+
+    train_dataset, validation_dataset = floorplans.load_data()
+
+    trainer = unet.Trainer(checkpoint_callback=False)
+    trainer.fit(unet_model,
+                train_dataset,
+                validation_dataset,
+                epochs=2,
+                batch_size=1,
+                verbose=2)
+
+    unet_model.save("unet_model")
+
+if __name__ == "__main__":
+    tic = time.time()
+    main()
+    toc = time.time()
+    print('total training + evaluation time = {} minutes'.format((toc - tic) / 60))
