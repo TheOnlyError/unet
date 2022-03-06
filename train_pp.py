fd270102d57f9287d82a0738f265b9510c06646c
Gijs <gijssieboy007@live.nl>
2022-03-06 11:55:30 +0100

    s

--- train_pp.py
+++ train_pp.py
@@ -32,7 +32,7 @@ def main():
     trainer.fit(unet_model,
                 train_dataset,
                 validation_dataset,
-                epochs=120,
+                epochs=60,
                 batch_size=1,
                 verbose=2)
 
